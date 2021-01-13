90b1216ba3134b2a052e39876ab2652a72d154c8
thanhdt246 <dothanh24062000@gmail.com>
2021-01-13 08:20:39 +0700

    create project

---
+++ src/test/java/com/codegym/airbnb/AirbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
