a9e46d705c2ee874ca22728eb6a5baed0fd42b7f
thanhdt246 <dothanh24062000@gmail.com>
2021-01-13 08:48:17 +0700

    create province model

---
+++ src/main/java/com/codegym/airbnb/model/Province.java
@@ -0,0 +1,22 @@
+package com.codegym.airbnb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Province {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String type;
+}
