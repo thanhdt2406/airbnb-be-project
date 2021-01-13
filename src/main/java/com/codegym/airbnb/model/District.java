16805071a6e9c27607fe3302b4a12aef1d73b1a4
thanhdt246 <dothanh24062000@gmail.com>
2021-01-13 08:50:23 +0700

    create district model

---
+++ src/main/java/com/codegym/airbnb/model/District.java
@@ -0,0 +1,24 @@
+package com.codegym.airbnb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class District {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    @ManyToOne
+    private Province province;
+}
