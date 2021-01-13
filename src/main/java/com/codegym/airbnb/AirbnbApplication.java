8819cc7f62544f4004562e79adf1c7a1d4047b9c
thanhdt246 <dothanh24062000@gmail.com>
2021-01-13 11:13:23 +0700

    create User repo

--- src/main/java/com/codegym/airbnb/AirbnbApplication.java
+++ src/main/java/com/codegym/airbnb/AirbnbApplication.java
@@ -8,6 +8,5 @@ public class AirbnbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AirbnbApplication.class, args);
-        System.out.println("viet");
     }
 }
---
+++ src/main/java/com/codegym/airbnb/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.codegym.airbnb.repository;
+
+import com.codegym.airbnb.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+    User findByUsername(String username);
+}
