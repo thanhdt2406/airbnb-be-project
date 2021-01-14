1277ec5119a53f97a318f2985d3623a59f81913f
thanhdt246 <dothanh24062000@gmail.com>
2021-01-14 16:36:32 +0700

    delete 1 import

--- src/main/java/com/codegym/airbnb/controller/AuthController.java
+++ src/main/java/com/codegym/airbnb/controller/AuthController.java
@@ -12,7 +12,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
