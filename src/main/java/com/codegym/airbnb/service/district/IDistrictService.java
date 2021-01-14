390663ffe4bea23662838aeb9b56f89d4e6e49e5
gintk111 <thanhnguyentz1997@gmail.com>
2021-01-14 15:39:59 +0700

    api dis

--- src/main/java/com/codegym/airbnb/service/district/IDistrictService.java
+++ src/main/java/com/codegym/airbnb/service/district/IDistrictService.java
@@ -1,7 +1,9 @@
 package com.codegym.airbnb.service.district;
 
 import com.codegym.airbnb.model.District;
+import com.codegym.airbnb.model.Province;
 import com.codegym.airbnb.service.IGeneralService;
 
 public interface IDistrictService extends IGeneralService<District> {
+    Iterable<District> findAllByProvince(Province province);
 }
