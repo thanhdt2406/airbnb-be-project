1c70036c5ed377025a15f8e278d8621e0809fd66
gintk111 <thanhnguyentz1997@gmail.com>
2021-01-17 00:06:47 +0700

    update model date-time comment

--- src/main/java/com/codegym/airbnb/model/Comment.java
+++ src/main/java/com/codegym/airbnb/model/Comment.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.sql.Timestamp;
 
 @Entity
 @Table
@@ -21,6 +22,9 @@ public class Comment {
     @NotNull
     @Size(max = 1000)
     private String content;
+
+    @Column(name = "timestamp", nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp date;
 
     @ManyToOne
     private User user;
